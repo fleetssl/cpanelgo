dea34faf01fa4f7476abccbcbf13cfcc81218e09
Alex Zorin <alex@zor.io>
2017-02-10 09:58:16 +1100

    cpanel: add FetchZoneRecords

--- cpanel/zoneedit.go
+++ cpanel/zoneedit.go
@@ -10,6 +10,9 @@ type ZoneRecord struct {
 	Name   string `json:"name"`
 	Record string `json:"record"`
 	Type   string `json:"type"`
+	Raw    string `json:"raw"`
+	TTL    string `json:"raw"`
+	Serial string `json:"serial"`
 }
 
 type FetchZoneApiResponse struct {
@@ -27,6 +30,28 @@ func (c CpanelApi) FetchZone(domain, types string) (FetchZoneApiResponse, error)
 		"domain": domain,
 		"type":   types, // can be multiple CNAME,A,AAAA
 	}, &out)
+
+	if err == nil && out.Event.Result != 1 {
+		err = errors.New(out.Event.Reason)
+	}
+
+	return out, err
+}
+
+type FetchZoneRecordsApiResponse struct {
+	cpanelgo.BaseAPI2Response
+	Data []ZoneRecord `json:"data"`
+}
+
+func (c CpanelApi) FetchZoneRecords(domain string, args cpanelgo.Args) (FetchZoneRecordsApiResponse, error) {
+	var out FetchZoneRecordsApiResponse
+
+	if args == nil {
+		args = cpanelgo.Args{}
+	}
+	args["domain"] = domain
+
+	err := c.Gateway.API2("ZoneEdit", "fetchzone_records", args, &out)
 
 	if err == nil && out.Event.Result != 1 {
 		err = errors.New(out.Event.Reason)
