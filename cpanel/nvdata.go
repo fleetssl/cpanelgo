ceda8bb359f5e2d1481dbde01cd8b0382202d6b3
Isaac <git@eggsampler.com>
2017-06-28 00:57:41 +1000

    Generified NVData structures

--- cpanel/nvdata.go
+++ cpanel/nvdata.go
@@ -6,28 +6,6 @@ import (
 	"github.com/letsencrypt-cpanel/cpanelgo"
 )
 
-const NVDatastoreName = "letsencrypt-cpanel"
-
-type NVDataDomainCerts struct {
-	Domain     string   `json:"domain"`
-	CertUrl    string   `json:"url"`
-	DomainKey  string   `json:"key"`
-	DomainCert string   `json:"cert"`
-	IssuerCert string   `json:"issuer"`
-	KeyId      string   `json:"key_id"`  // the cpanel key_id from install_ssl
-	CertId     string   `json:"cert_id"` // the cpanel cert_id from install_ssl
-	CertExpiry int64    `json:"cert_expiry"`
-	AltNames   []string `json:"alt_names"`
-}
-
-type NVDataAccount struct {
-	AccountKey       string                        `json:"accountkey"`
-	LastRenewalCheck int64                         `json:"last_renewal_check"`
-	Certs            map[string]*NVDataDomainCerts `json:"certs"`
-	DisableMail      bool                          `json:"disable_mail"`
-	AllowAutoSSL     bool                          `json:"allow_auto_ssl"`
-}
-
 type NVDataGetApiResult struct {
 	cpanelgo.BaseUAPIResponse
 	Data []struct {
@@ -36,52 +14,35 @@ type NVDataGetApiResult struct {
 	} `json:"data"`
 }
 
-func (a *NVDataAccount) AllNames() []string {
-	out := []string{}
-	for _, v := range a.Certs {
-		out = append(out, append(v.AltNames, v.Domain)...)
-	}
-	return out
-}
-
-func (c CpanelApi) GetNVData() (*NVDataAccount, error) {
+func (c CpanelApi) GetNVData(name string) (NVDataGetApiResult, error) {
 	var out NVDataGetApiResult
 	err := c.Gateway.UAPI("NVData", "get", cpanelgo.Args{
-		"names": NVDatastoreName,
+		"names": name,
 	}, &out)
 	if err == nil {
 		err = out.Error()
 	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	var acct NVDataAccount
-	if len(out.Data) == 0 {
-		return &acct, nil
-	}
-
-	if len(out.Data[0].FileContents) == 0 {
-		return &acct, nil
-	}
-
-	err = json.Unmarshal([]byte(out.Data[0].FileContents), &acct)
-
-	return &acct, err
+	return out, err
 }
 
-func (c CpanelApi) SetNVData(data *NVDataAccount) (cpanelgo.BaseAPI2Response, error) {
-	var out cpanelgo.BaseAPI2Response
+type NVDataSetApiResult struct {
+	cpanelgo.BaseUAPIResponse
+	Data []struct {
+		Set string `json:"set"`
+	} `json:"data"`
+}
+
+func (c CpanelApi) SetNVData(name string, data interface{}) (NVDataSetApiResult, error) {
+	var out NVDataSetApiResult
 
 	buf, err := json.Marshal(data)
 	if err != nil {
 		return out, err
 	}
 
-	err = c.Gateway.API2("NVData", "set", cpanelgo.Args{
-		"names":         NVDatastoreName,
-		NVDatastoreName: string(buf),
+	err = c.Gateway.UAPI("NVData", "set", cpanelgo.Args{
+		"names": name,
+		name:    string(buf),
 	}, &out)
 
 	if err == nil {
